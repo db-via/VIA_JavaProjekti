c4d09b841c0e08553c04312d3435e84c8ad11002
Dāvis Birzgalis <davis.birzgalis@va.lv>
2025-10-18 13:00:34 +0300

    Added Main Menu and Default switch choice

--- inheritance_and_abstraction/FifthProject.java
+++ inheritance_and_abstraction/FifthProject.java
@@ -1,10 +1,44 @@
 package inheritance_and_abstraction;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class FifthProject {
 
 	public static void main(String[] args) {
 		
-
+		Scanner obj = new Scanner(System.in);
+		
+		while (true) {
+			System.out.println("(|Supermarket Inventory System|)");
+			System.out.println("1. Add Perishable Product");
+			System.out.println("2. Add Non-Perishable Product");
+			System.out.println("3. Display Product Info");
+			System.out.println("4. Restock Product");
+			System.out.println("5. Exit");
+			int menu_choice = 0;
+			
+            try {
+                menu_choice = obj.nextInt();
+                obj.nextLine(); // Consume newline
+            } catch (InputMismatchException e) {
+                System.out.println("HALT! Please enter a NUMBER.");
+                obj.next(); // Discards invalid input
+                continue; // Goes back to main menu
+            }
+            
+            switch (menu_choice) {
+            	case 1:
+            		System.out.println();
+            		break;
+            	case 5:
+            		System.out.println("Bye!");
+            		return;
+            	default:
+            		System.out.println("YOU STUPID!");
+            		break;
+            }
+			
+		}
 	}
-
 }
